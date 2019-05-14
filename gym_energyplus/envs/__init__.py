7117fe6caa048fe0bc687c7329c737270d1554ee
xiaonanchong <xnchong@outlook.com>
2019-05-14 10:13:32 +0100

    Update __init__.py

--- gym_energyplus/envs/__init__.py
+++ gym_energyplus/envs/__init__.py
@@ -1,2 +1,2 @@
 from gym_energyplus.envs.energyplus_env import EnergyplusEnv
-from gym_energyplus.envs.energyplus_extrahard_env import EnergyplusExtraHardEnv
+#from gym_energyplus.envs.energyplus_extrahard_env import EnergyplusExtraHardEnv
